5d3a89d8c7b49aed3da5b57c79c131e829da895c
Glitch (type-tester-venice) <none>
2018-09-12 08:57:34 +0000

    🎲📍 Updated with Glitch

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-import domready from 'domready';
+import "./main.css";
 
-import style from "./main.css";
-
-const getAllValues = (elements) => elements.map(el => el.value);
-const getArticleEl = () => document.querySelector('.article')
-
-domready(() => {
-	const buttons = Array.from(document.querySelectorAll('.style-selector__btn'));
-
-	buttons.forEach(btn => {
-		btn.addEventListener('click', e => {
-			const articleEl = getArticleEl();
-			getAllValues(buttons).forEach(name => articleEl.classList.remove(`article--type-${name}`));
-
-			articleEl.classList.add(`article--type-${e.target.value}`)
-		});
-	});
-});
-
+console.log('🚀');
